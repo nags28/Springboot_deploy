abeb6e23cd1f02ea7813e5f0560175a29b84eec3
nagarjuna n <nagarjuna.n@sonata-software.com>
2020-05-14 16:58:35 +0530

    deleted files

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "prefix" {
-  description = "The Prefix used for all resources in this example"
-  default = "demo"
- }
-
-variable "location" {
-  description = "The Azure Region in which the resources in this example should exist"
-  default = "East US" 
-}
